df80dd28144e752ae68731f4251780436a53bfb8
Rattl3bones <42248595+Rattl3bones@users.noreply.github.com>
2020-04-01 19:10:18 -0400

    Added Player and Rocket Script

---
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+	// Use this for initialization
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKeyDown("up") || Input.GetKeyDown("space"))
+            while (!Input.GetKeyUp("up") || !Input.GetKeyUp("space"))
+            {
+                rigidBody.AddRelativeForce(Vector3.up);
+            }
+                
+
+        else if (Input.GetKeyDown("down"))
+            print("Down");
+        else if (Input.GetKeyDown("left"))
+            print("Left");
+        else if (Input.GetKeyDown("right"))
+            print("Right");
+    }
+}
