90d175fe6888cfb311043efb08e7a1929adf21c9
Rattl3bones <42248595+Rattl3bones@users.noreply.github.com>
2020-04-05 19:58:59 -0400

    Big fixes and polish

--- Project Boost/Assets/Scripts/Rocket.cs
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 	// Use this for initialization
 	void Start ()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -20,18 +22,41 @@ public class Rocket : MonoBehaviour
 
     private void ProcessInput()
     {
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
-            rigidBody.AddRelativeForce(Vector3.up);
+        thrust();
+        rotation();
+
+    }
+
+    private void rotation()
+    {
+
+        rigidBody.freezeRotation = true;
 
         if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            float rotationSpeed = 
             transform.Rotate(Vector3.forward);
+        }
 
         if (Input.GetKey(KeyCode.RightArrow))
+        {
             transform.Rotate(-Vector3.forward);
+        }
 
-        else if (Input.GetKeyDown("down"))
-            print("Down");
-    
-       
+        rigidBody.freezeRotation = false;
+    }
+
+    private void thrust()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
     }
 }
